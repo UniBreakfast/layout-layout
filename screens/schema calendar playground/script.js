cb73c5368eea0276988e214573a4a33d8dcdffaf
unibreakfast <unibreakfast1@gmail.com>
2020-07-19 22:31:25 +0300

    do breaks and rest

--- screens/schema calendar playground/script.js
+++ screens/schema calendar playground/script.js
@@ -1,7 +1,9 @@
-schemaBtns.append(buildEmptySchemaBtn(), ...(JSON.parse(localStorage.schemas || '""') || schemas).filter(schema => schema!=`{
+const schema0 = `{
   title: "",
 
-}`).map(buildSchemaBtn), buildEmptySchemaBtn())
+}`
+
+schemaBtns.append(buildEmptySchemaBtn(), ...(JSON.parse(localStorage.schemas || '""') || schemas).filter(schema => schema!=schema0).map(buildSchemaBtn), buildEmptySchemaBtn())
 
 let dateObj = new Date('2020')
 
@@ -17,7 +19,7 @@ do { //* BUILD CALENDAR BUTTONS
   if (date=='01') calendar.append(mon)
   calendar.append(btn)
   dateObj.setDate(dateObj.getDate() + 1)
-  if (day == 'Sun') week = week=='odd'? 'even':'odd'
+  if (day == 'Sun') week = week? '':'odd'
 } while (document.body.scrollHeight < innerHeight)
 
 //* REMOVE EXCESSIVE BUTTONS
@@ -41,43 +43,71 @@ setTimeout(()=> schemaBtns.querySelector('pre').click())
 
 
 schemaBtns.onclick = e => {
-  if (e.target.tagName=='PRE') {
+  if (e.target.tagName=='PRE' || e.target.firstChild?.tagName=='PRE') {
     if (e.altKey) {
       const ta = document.createElement('textarea')
       ta.value = e.target.innerText
       e.target.replaceWith(ta)
+      fixSize(ta)
       ta.focus()
-    } else if (e.ctrlKey + e.shiftKey) {
+    } else if (e.ctrlKey && e.shiftKey) {
       e.stopPropagation()
       e.preventDefault()
       e.target.parentNode.remove()
       localStorage.schemas = JSON.stringify([...schemaBtns.querySelectorAll('pre, textarea')].map(el => el.innerText || el.value))
     } else
-      plan(100, eval(`(${e.target.innerText})`))
+      plan(500, eval(`(${e.target.innerText})`))
   }
 }
 
 schemaBtns.onkeydown = e => {
-  if (e.target.tagName=='TEXTAREA' &&
-    (e.key=='Enter' && e.ctrlKey || e.key=='Escape')) {
-    const pre = document.createElement('pre')
-    pre.innerText = e.target.value
-    e.target.replaceWith(pre)
-    localStorage.schemas = JSON.stringify([...schemaBtns.querySelectorAll('pre, textarea')].map(el => el.innerText || el.value))
-    if (!schemaBtns.firstChild.querySelector('textarea'))
-      schemaBtns.prepend(buildEmptySchemaBtn())
-    if (!schemaBtns.lastChild.querySelector('textarea'))
-      schemaBtns.append(buildEmptySchemaBtn())
+  if (e.target.tagName=='TEXTAREA') {
+    const ta = e.target
+    if (e.key=='Enter' && e.ctrlKey || e.key=='Escape') {
+      const pre = document.createElement('pre')
+      pre.innerText = ta.value
+      ta.replaceWith(pre)
+      localStorage.schemas = JSON.stringify([...schemaBtns.querySelectorAll('pre, textarea')].map(el => el.innerText || el.value))
+      if (!schemaBtns.firstChild.querySelector('textarea'))
+        schemaBtns.prepend(buildEmptySchemaBtn())
+      if (!schemaBtns.lastChild.querySelector('textarea'))
+        schemaBtns.append(buildEmptySchemaBtn())
+    } else if (e.key=='Tab') {
+      e.preventDefault()
+      const pos = ta.selectionStart
+      ta.value = ta.value.slice(0, pos) + '  ' + ta.value.slice(pos)
+      ta.selectionStart = ta.selectionEnd = pos+2
+    } else if (e.key=='ArrowLeft' && e.ctrlKey
+      && ta.parentNode.previousSibling) {
+        schemaBtns.insertBefore(ta.parentNode, ta.parentNode.previousSibling)
+        e.preventDefault()
+        ta.focus()
+    } else if (e.key=='ArrowRight' && e.ctrlKey
+      && ta.parentNode.nextSibling) {
+        schemaBtns.insertBefore(ta.parentNode.nextSibling, ta.parentNode)
+        e.preventDefault()
+        ta.focus()
+    } else fixSize(ta)
+  } else {
+    const btn = e.target
+    if (e.key=='ArrowLeft' && e.ctrlKey && btn.previousSibling) {
+      schemaBtns.insertBefore(btn, btn.previousSibling)
+      e.preventDefault()
+      btn.focus()
+    } else if (e.key=='ArrowRight' && e.ctrlKey && btn.nextSibling) {
+      schemaBtns.insertBefore(btn.nextSibling, btn)
+      e.preventDefault()
+      btn.focus()
+    }
   }
+
 }
 
 function buildEmptySchemaBtn() {
   const btn = document.createElement('button')
   const ta = document.createElement('textarea')
-  ta.value = `{
-  title: "",
-
-}`
+  ta.value = schema0
+  ta.cols = 13, ta.rows = 5
   btn.append(ta)
   return btn
 }
@@ -91,21 +121,27 @@ function buildSchemaBtn(schema) {
 }
 
 function render() {
-  calendar.querySelectorAll('.planned').forEach(btn =>
-    btn.classList.remove('planned'))
-  dodays.forEach(date =>
-    calendar.querySelector(`[title="${date}"]`)?.classList.add('planned'))
+  calendar.querySelectorAll('.planned, .even').forEach(btn =>
+    btn.classList.remove('planned', 'even'))
+  dodays.forEach(day => {
+    const btn = calendar.querySelector(`[title="${day.date}"]`)
+    if (btn) {
+      btn.classList.add('planned')
+      if (day.group%2) btn.classList.add('even')
+    }
+  })
 }
 
 function plan(n/* days */, s/* schema */, dayLimit=999) {
-  const days = s.days.length? s.days : [0,1,2,3,4,5,6]
+  const days = s.days?.length? s.days : [0,1,2,3,4,5,6]
   dateObj = new Date('2020')
   dodays.length = 0
+  let i = 0
   if (s.days) {
     while (dodays.length < n && dayLimit--) {
-      const weekday = (dateObj.getDay()+6)%7
       const [day, mon, date, year] = dateObj.toString().split(' ')
       const month = String(dateObj.getMonth() + 1).padStart(2, 0)
+      const weekday = (dateObj.getDay()+6)%7
       if (days.includes(weekday))
         if (s.skip)
           if (s.do) ;
@@ -122,7 +158,42 @@ function plan(n/* days */, s/* schema */, dayLimit=999) {
   } else if (s.dates) {
 
   } else {
-
+    let rest = 0,  breaks = 0
+    while (dodays.length < n && dayLimit--) {
+      const [day, mon, date, year] = dateObj.toString().split(' ')
+      const month = String(dateObj.getMonth() + 1).padStart(2, 0)
+      const weekday = (dateObj.getDay()+6)%7
+      const doLength = s.do || 1
+      const j = i%doLength
+      if (i && !j) {
+        if (breaks < s.breaks) breaks++
+        else if (rest < s.rest) rest++
+        else {
+          breaks = rest = 0
+          dodays.push(new DoDay(`${year}-${month}-${date}`,
+            i, i/doLength|0, j))
+          i++
+        }
+      } else {
+        dodays.push(new DoDay(`${year}-${month}-${date}`,
+          i, i/doLength|0, j))
+        i++
+      }
+      dateObj.setDate(dateObj.getDate()+1)
+    }
   }
   render()
+}
+
+function fixSize(textarea) {
+  setTimeout(()=> {
+    const lines = textarea.value.split('\n')
+    textarea.cols = 0| 1.053*
+      Math.max(...lines.map(line => line.length))
+    textarea.rows = lines.length+1
+  })
+}
+
+function DoDay(date, i, group, j) {
+  Object.assign(this, {date, i, group, j})
 }
